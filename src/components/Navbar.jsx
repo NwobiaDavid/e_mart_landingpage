ec6f2fcf05af36cdfdfd3205990bda1e4ebad599
NwobiaDavid <dnwobia@gmail.com>
2023-09-13 07:47:45 +0100

    made the navbar mobile friendly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { BsTelephone } from "react-icons/bs";
 
 export default function Navbar() {
   return (
-    <div className="flex h-16 border-2 border-b-green-500 justify-between items-center pl-24">
-      <div className="text-3xl">
-          <span className="p-2 font-light header-font"> ebonyi </span>
-          <span className="p-2 font-bold"> e-Mart </span>
+    <div className="flex h-16 border-2 border-b-green-500 justify-between items-center md:pl-24">
+      <div className=" md:text-3xl px-2 text-xl">
+          <span className="md:p-2 font-light header-font"> ebonyi</span>
+          <span className="md:p-2 font-bold">e-Mart </span>
       </div>
-      <div className="px-10">
-          <div className="flex text-lg items-center justify-center">
+      <div className="md:px-10 px-1">
+          <div className="flex text-sm md:text-lg items-center justify-center">
               <BsTelephone />
               (+234)90401874321
           </div>
