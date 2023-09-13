e5f0ba74d6c656c1fb4316e955ed753d8339bc83
NwobiaDavid <dnwobia@gmail.com>
2023-09-13 08:01:46 +0100

    made one.js mobile friendly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ export default function Navbar() {
   return (
     <div className="flex h-16 border-2 border-b-green-500 justify-between items-center md:pl-24">
       <div className=" md:text-3xl px-2 text-xl">
-          <span className="md:p-2 font-light header-font"> ebonyi</span>
+          <span className="md:p-2 font-light header-font"> ebonyi </span>
           <span className="md:p-2 font-bold">e-Mart </span>
       </div>
       <div className="md:px-10 px-1">
           <div className="flex text-sm md:text-lg items-center justify-center">
-              <BsTelephone />
+              <BsTelephone /> 
               (+234)90401874321
           </div>
       </div>
--- src/components/One.jsx
+++ src/components/One.jsx
@@ -11,9 +11,9 @@ export default function One() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 h-[750px] ">
 
-      <div className="md:py-4 md:px-10 ">
+      <div className="md:py-4 text-center md:text-left md:px-10 px-2 ">
         <h1 className="text-4xl p-3">Introducing ebonyi e-mart</h1>
-        <p className="text-lg leading-7 break-keep">
+        <p className="text-lg md:leading-7 ">
          <span className="text-2xl font-semibold pl-4">W</span>elcome to ebonyi e-Mart, your one-stop solution for
           convenient and hassle-free grocery shopping. Say goodbye to long
           crowded markets and costly transport fare – we bring the market to your
@@ -23,7 +23,7 @@ export default function One() {
           what matters most.
         </p>
         <h1  className="text-4xl p-3"> What We Do</h1>
-        <p className="text-lg leading-8 break-keep antialiased tracking-wider">
+        <p className="text-xl leading-8  md:text-lg md:leading-8  antialiased md:tracking-wider">
         <span className="text-2xl pl-4 font-semibold">A</span>t ebonyi e-Mart, We specialize in delivering fresh, high-quality groceries and food
           directly to your door. Our dedicated team ensures you receive
           carefully selected products tailored to your preferences and dietary
@@ -49,15 +49,16 @@ export default function One() {
               This campaign only lasts for one week, so act fast.
             </p>
           </div>
-          <div className="bg-gray-100 mt-3 rounded-lg w-full flex items-center justify-center h-[26%] ">
-            <form action="#" className="relative w-[50%]" method="post">
+
+          <div className="md:bg-gray-100 mt-3 rounded-lg w-full flex items-center justify-center h-[50%] md:h-[26%] ">
+            <form action="#" className="relative w-full " method="post">
               <input
-                className="outline-none w-full rounded-full px-3 py-3"
+                className="outline-none border-2 border-orange-400 duration-200 focus:border-orange-600 w-full rounded-full px-3 py-2 md:py-3"
                 type="email"
                 placeholder="your email..."
               />
               <button
-                className="z-10 right-0 px-2 rounded-r-full text-xl duration-200 w-[7rem] h-full bg-orange-400 text-white hover:bg-orange-500 absolute"
+                className="z-10 right-0 px-2 rounded-r-full text-sm md:text-xl duration-200 w-[4rem] md:w-[7rem] h-full bg-orange-400 text-white hover:bg-orange-600 absolute"
                 type="submit"
               >
                 Act Now
